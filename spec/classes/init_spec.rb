4f8bd7fc412eb54327bb9a3b28649e1d46a519f8
Steven Pritchard <steven.pritchard@gmail.com>
2017-05-08 17:55:58 -0400

    Add facts.

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -2,6 +2,10 @@ require 'spec_helper'
 
 describe 'zram' do
   context 'with defaults for all parameters' do
+    let(:facts) {{
+      :processorcount => '4',
+      :memorysize_mb  => '4096',
+    }}
     it { is_expected.to compile }
     it { is_expected.to contain_class('zram') }
     it { is_expected.to contain_file('/lib/udev/zram').with(
@@ -12,6 +16,6 @@ describe 'zram' do
     )}
     it { is_expected.to contain_file('/etc/udev/rules.d/01-zram.rules').with(
       'ensure' => 'file',
-    ).with_content(/^KERNEL=="zram\*", ACTION=="add", RUN=="\/lib\/udev\/zram \d+"$/)}
+    ).with_content(/^KERNEL=="zram\*", ACTION=="add", RUN=="\/lib\/udev\/zram 536870912"$/)}
   end
 end
