0ddef48a9f365923ff0625ca2aefe324e87161ef
Steven Pritchard <steven.pritchard@gmail.com>
2017-05-08 18:16:45 -0400

    Modify rspec tests to use rspec-puppet-facts.

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -1,22 +1,26 @@
 require 'spec_helper'
 
 describe 'zram' do
-  context 'with defaults for all parameters' do
-    let(:facts) {{
-      :processorcount => '4',
-      :memorysize_mb  => '4096',
-      :augeasversion  => '1.4.0',
-    }}
-    it { is_expected.to compile }
-    it { is_expected.to contain_class('zram') }
-    it { is_expected.to contain_file('/lib/udev/zram').with(
-      'ensure' => 'file',
-      'owner'  => 'root',
-      'group'  => 'root',
-      'mode'   => '0755',
-    )}
-    it { is_expected.to contain_file('/etc/udev/rules.d/01-zram.rules').with(
-      'ensure' => 'file',
-    ).with_content(/^KERNEL=="zram\*", ACTION=="add", RUN=="\/lib\/udev\/zram 536870912"$/)}
+  on_supported_os.each do |os, facts|
+    context "on #{os} with defaults for all parameters" do
+      let(:facts) do
+        facts.merge({
+          :processorcount => '4',
+          :memorysize_mb  => '4096',
+          :augeasversion  => '1.4.0',
+        })
+      end
+      it { is_expected.to compile }
+      it { is_expected.to contain_class('zram') }
+      it { is_expected.to contain_file('/lib/udev/zram').with(
+        'ensure' => 'file',
+        'owner'  => 'root',
+        'group'  => 'root',
+        'mode'   => '0755',
+      )}
+      it { is_expected.to contain_file('/etc/udev/rules.d/01-zram.rules').with(
+        'ensure' => 'file',
+      ).with_content(/^KERNEL=="zram\*", ACTION=="add", RUN=="\/lib\/udev\/zram 536870912"$/)}
+    end
   end
 end
