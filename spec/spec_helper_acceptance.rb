cf5bb9c678aa7d4b0a0cef7b0beae45653cef408
Steven Pritchard <steven.pritchard@gmail.com>
2017-05-09 14:29:18 -0500

    Install dependencies explicitly instead of using install_module_dependencies_on(hosts).

--- spec/spec_helper_acceptance.rb
+++ spec/spec_helper_acceptance.rb
@@ -5,5 +5,6 @@ require 'beaker/module_install_helper'
 
 run_puppet_install_helper
 install_module_on(hosts)
-install_module_dependencies_on(hosts)
-#install_module_from_forge_on(hosts, 'puppetlabs-stdlib', '>= 4.13.0 < 5.0.0')
+#install_module_dependencies_on(hosts)
+install_module_from_forge_on(hosts, 'puppetlabs-stdlib', '>= 4.13.0 < 5.0.0')
+install_module_from_forge_on(hosts, 'camptocamp-kmod', '>= 2.1.0 < 3.0.0')
