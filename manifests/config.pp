a192882963bce561c9a6daadd1dff23f50964fed
Steven Pritchard <steven.pritchard@onyxpoint.com>
2019-04-24 08:31:21 -0500

    Use `content` instead of `source`.

--- manifests/config.pp
+++ manifests/config.pp
@@ -11,11 +11,11 @@ class zram::config {
   assert_private()
 
   file { '/lib/udev/zram':
-    ensure => file,
-    owner  => 'root',
-    group  => 'root',
-    mode   => '0755',
-    source => 'puppet:///modules/zram/zram.sh',
+    ensure  => file,
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0755',
+    content => file('zram/zram.sh'),
   }
 
   file { '/etc/udev/rules.d/01-zram.rules':
